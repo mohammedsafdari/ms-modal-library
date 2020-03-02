d9ca923c02863c0bd64f6075d514ee7fc3460303
Mohammed Safdari <mohammed.safdari@zensar.com>
2020-03-02 18:52:38 +0530

    Initial Commit

---
+++ projects/modal-lib/src/lib/modal-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
+
+
+
+@NgModule({
+  declarations: [ModalComponent],
+  imports: [
+  ],
+  exports: [ModalComponent]
+})
+export class ModalLibModule { }
---
+++ projects/modal-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of modal-lib
+ */
+
+export * from './lib/modal-lib.module';
+export * from './lib/services/modal.service';
